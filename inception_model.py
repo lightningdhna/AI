642966dbf74b833586e4b56314a4f5b504f152de
hung <shalightdowdh@gmail.com>
2023-07-16 02:36:58 +0700

    change model

--- inception_model.py
+++ inception_model.py
@@ -11,18 +11,6 @@ from keras import layers, backend, models
 import tensorflow as tf
 
 
-def preprocess_input(x, **kwargs):
-    """Preprocesses a numpy array encoding a batch of images.
-
-    # Arguments
-        x: a 4D numpy array consists of RGB values within [0, 255].
-
-    # Returns
-        Preprocessed array.
-    """
-    return imagenet_utils.preprocess_input(x, mode='tf', **kwargs)
-
-
 def conv2d_bn(x,
               filters,
               kernel_size,
@@ -51,24 +39,24 @@ def conv2d_bn(x,
 
 def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
     if block_type == 'block35':
-        branch_0 = conv2d_bn(x, 32, 1)
-        branch_1 = conv2d_bn(x, 32, 1)
-        branch_1 = conv2d_bn(branch_1, 32, 3)
-        branch_2 = conv2d_bn(x, 32, 1)
-        branch_2 = conv2d_bn(branch_2, 48, 3)
-        branch_2 = conv2d_bn(branch_2, 64, 3)
+        branch_0 = conv2d_bn(x, 4, 1)
+        branch_1 = conv2d_bn(x, 4, 1)
+        branch_1 = conv2d_bn(branch_1, 4, 3)
+        branch_2 = conv2d_bn(x, 4, 1)
+        branch_2 = conv2d_bn(branch_2, 6, 3)
+        branch_2 = conv2d_bn(branch_2, 8, 3)
         branches = [branch_0, branch_1, branch_2]
     elif block_type == 'block17':
-        branch_0 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(x, 128, 1)
-        branch_1 = conv2d_bn(branch_1, 160, [1, 7])
-        branch_1 = conv2d_bn(branch_1, 192, [7, 1])
+        branch_0 = conv2d_bn(x, 24, 1)
+        branch_1 = conv2d_bn(x, 16, 1)
+        branch_1 = conv2d_bn(branch_1, 20, [1, 7])
+        branch_1 = conv2d_bn(branch_1, 24, [7, 1])
         branches = [branch_0, branch_1]
     elif block_type == 'block8':
-        branch_0 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(x, 192, 1)
-        branch_1 = conv2d_bn(branch_1, 224, [1, 3])
-        branch_1 = conv2d_bn(branch_1, 256, [3, 1])
+        branch_0 = conv2d_bn(x, 24, 1)
+        branch_1 = conv2d_bn(x, 24, 1)
+        branch_1 = conv2d_bn(branch_1, 28, [1, 3])
+        branch_1 = conv2d_bn(branch_1, 32, [3, 1])
         branches = [branch_0, branch_1]
     else:
         raise ValueError('Unknown Inception-ResNet block type. '
@@ -98,64 +86,68 @@ def inception_resnet_block(x, scale, block_type, block_idx, activation='relu'):
 def InceptionResNetV2(input_shape):
     img_input = keras.Input(shape=input_shape)
     # Stem block: 35 x 35 x 192
-    x = conv2d_bn(img_input, 32, 3, strides=2, padding='valid')
-    x = conv2d_bn(x, 32, 3, padding='valid')
-    x = conv2d_bn(x, 64, 3)
-    x = layers.MaxPooling2D(3, strides=2)(x)
-    x = conv2d_bn(x, 80, 1, padding='valid')
-    x = conv2d_bn(x, 192, 3, padding='valid')
-    x = layers.MaxPooling2D(3, strides=2)(x)
+    x = conv2d_bn(img_input, 4, 3, strides=2, padding='valid')
+    # x = conv2d_bn(x, 4, 3, padding='valid')
+    # x = conv2d_bn(x, 8, 3)
+    # x = layers.AveragePooling2D(4, strides=2, padding='same')(x)
+    # x = conv2d_bn(x, 10, 1, padding='valid')
+    # x = conv2d_bn(x, 24, 3, padding='valid')
+    # x = layers.MaxPooling2D(3, strides=2)(x)
 
     # Mixed 5b (Inception-A block): 35 x 35 x 320
-    branch_0 = conv2d_bn(x, 96, 1)
-    branch_1 = conv2d_bn(x, 48, 1)
-    branch_1 = conv2d_bn(branch_1, 64, 5)
-    branch_2 = conv2d_bn(x, 64, 1)
-    branch_2 = conv2d_bn(branch_2, 96, 3)
-    branch_2 = conv2d_bn(branch_2, 96, 3)
+    branch_0 = conv2d_bn(x, 12, 1)
+    branch_1 = conv2d_bn(x, 6, 1)
+    branch_1 = conv2d_bn(branch_1, 8, 5)
+    branch_2 = conv2d_bn(x, 8, 1)
+    branch_2 = conv2d_bn(branch_2, 12, 3)
+    branch_2 = conv2d_bn(branch_2, 12, 3)
     branch_pool = layers.AveragePooling2D(3, strides=1, padding='same')(x)
-    branch_pool = conv2d_bn(branch_pool, 64, 1)
+    branch_pool = conv2d_bn(branch_pool, 8, 1)
     branches = [branch_0, branch_1, branch_2, branch_pool]
     channel_axis = 1 if backend.image_data_format() == 'channels_first' else 3
     x = layers.Concatenate(axis=channel_axis, name='mixed_5b')(branches)
 
+    icb_nums=[5,10,15]
     # 10x block35 (Inception-ResNet-A block): 35 x 35 x 320
-    for block_idx in range(1, 11):
+    for block_idx in range(0,icb_nums[0]):
         x = inception_resnet_block(x,
                                    scale=0.17,
                                    block_type='block35',
                                    block_idx=block_idx)
 
     # Mixed 6a (Reduction-A block): 17 x 17 x 1088
-    branch_0 = conv2d_bn(x, 384, 3, strides=2, padding='valid')
-    branch_1 = conv2d_bn(x, 256, 1)
-    branch_1 = conv2d_bn(branch_1, 256, 3)
-    branch_1 = conv2d_bn(branch_1, 384, 3, strides=2, padding='valid')
+    branch_0 = conv2d_bn(x, 48, 3, strides=2, padding='valid')
+    branch_1 = conv2d_bn(x, 32, 1)
+    branch_1 = conv2d_bn(branch_1, 32, 3)
+    branch_1 = conv2d_bn(branch_1, 48, 3, strides=2, padding='valid')
     branch_pool = layers.MaxPooling2D(3, strides=2, padding='valid')(x)
     branches = [branch_0, branch_1, branch_pool]
     x = layers.Concatenate(axis=channel_axis, name='mixed_6a')(branches)
+    print(x.shape)
 
     # 20x block17 (Inception-ResNet-B block): 17 x 17 x 1088
-    for block_idx in range(1, 21):
+    for block_idx in range(0, icb_nums[1]):
         x = inception_resnet_block(x,
                                    scale=0.1,
                                    block_type='block17',
                                    block_idx=block_idx)
 
+    print(x.shape)
+
     # Mixed 7a (Reduction-B block): 8 x 8 x 2080
-    branch_0 = conv2d_bn(x, 256, 1)
-    branch_0 = conv2d_bn(branch_0, 384, 3, strides=2, padding='valid')
-    branch_1 = conv2d_bn(x, 256, 1)
-    branch_1 = conv2d_bn(branch_1, 288, 3, strides=2, padding='valid')
-    branch_2 = conv2d_bn(x, 256, 1)
-    branch_2 = conv2d_bn(branch_2, 288, 3)
-    branch_2 = conv2d_bn(branch_2, 320, 3, strides=2, padding='valid')
+    branch_0 = conv2d_bn(x, 32, 1)
+    branch_0 = conv2d_bn(branch_0, filters=48, kernel_size=3, strides=2, padding='valid')
+    branch_1 = conv2d_bn(x, 32, 1)
+    branch_1 = conv2d_bn(branch_1, 36, 3, strides=2, padding='valid')
+    branch_2 = conv2d_bn(x, 32, 1)
+    branch_2 = conv2d_bn(branch_2, 36, 3)
+    branch_2 = conv2d_bn(branch_2, 40, 3, strides=2, padding='valid')
     branch_pool = layers.MaxPooling2D(3, strides=2, padding='valid')(x)
     branches = [branch_0, branch_1, branch_2, branch_pool]
     x = layers.Concatenate(axis=channel_axis, name='mixed_7a')(branches)
 
     # 10x block8 (Inception-ResNet-C block): 8 x 8 x 2080
-    for block_idx in range(1, 10):
+    for block_idx in range(0,icb_nums[0]):
         x = inception_resnet_block(x,
                                    scale=0.2,
                                    block_type='block8',
@@ -167,10 +159,10 @@ def InceptionResNetV2(input_shape):
                                block_idx=10)
 
     # Final convolution block: 8 x 8 x 1536
-    x = conv2d_bn(x, 1536, 1, name='conv_7b')
+    x = conv2d_bn(x, 192, 1, name='conv_7b')
 
     x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-    x = layers.Dense(256, activation='tanh')
+    x = layers.Dense(32, activation='tanh')(x)
     x = layers.Dense(1, activation='sigmoid', name='predictions')(x)
 
     # Create model.
@@ -181,7 +173,7 @@ def InceptionResNetV2(input_shape):
     return model
 
 
-def create_resnet_classifier(finger, input_shape=(64, 64, 3)):
+def create_inception_classifier(finger, input_shape=(64, 64, 3)):
     model = InceptionResNetV2(input_shape)
     model.save(os.path.join('models', 'inception_' + str(finger) + '.h5'))
     return model
